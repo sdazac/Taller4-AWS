42d57f4a32a70749eb32b2e597751d4c569d0f30
sdazac <104094499+sdazac@users.noreply.github.com>
2025-10-30 17:13:44 -0500

    Update ALLOWED_HOSTS to new IP address

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-46@*8ujr7s0nefa9*90_bk*cnyi^+^n_&kzf46zxl!h2^mn2+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.92.21.159']
+ALLOWED_HOSTS = ['3.87.169.55']
 
 
 # Application definition
